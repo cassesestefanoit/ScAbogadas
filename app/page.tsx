14ebddb2863896f71756911f30546b0626d0534a
cassesestefanoit <cassese.stefano.it@gmail.com>
2026-04-12 23:02:52 -0300

    fix: rename Comentarios.tsx to Comentario.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "./components/Hero";
 import Especialidades from "./components/Especialidades";
 import Nosotras from "./components/Nosotras";
-import Comentarios from "./components/comentarios"
+import Comentarios from "./components/Comentario"
 import Contactanos from "./components/Contactanos";
 
 export default function Home() {
